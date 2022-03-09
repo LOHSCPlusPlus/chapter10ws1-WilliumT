852619f51933afeebd5541091e5e7d77a6e9f893
WilliamTh12 <>
2022-03-09 17:16:13 +0000

    Ws1

--- Point.cpp
+++ Point.cpp
@@ -1,9 +1,48 @@
 #include "Point.h"
-
+#include <math.h>
+#include <iostream>
+using namespace std;
 // Notice how when we use field names in the functions that
 // belong to Point, we can just use the name without a dot operator.
-Point::Point() {
-    x = 0.0;
-    y = 0.0;
+//accessor
+//double Point::getX(){
+  //return x;
+//}
+//accesor
+//double Point::getY(){
+ // return y;
+//}
+void Point::print(){
+  cout << "x: " << x;
+  cout << "y: " << y;
+  cout << endl;
 }
 
+Point::Point(){
+  x = 0.0;
+  y = 0.0;
+  }
+
+Point::Point(double xVal, double yVal) {
+    x = xVal;
+    y = yVal;
+}
+
+double Point::calcDistance(Point other){
+
+  x = other.x;
+  y = other.y;
+  
+  double xVal = pow(x-other.x, 2);
+  double yVal = pow(y-other.y, 2);
+  double dist = sqrt(xVal+yVal);
+  return dist;
+}
+//mutator
+//void Point::setX(double xVal){
+ // x = xVal;
+//}
+//mutator
+//void Point::setY(double yVal){
+  //y = yVal;
+//}
--- Point.h
+++ Point.h
@@ -1,6 +1,14 @@
 class Point{
     public:
-        Point();
+Point();
+void print();
+double calcDistance(Point other);
+//double getX();
+//double getY();
+//void setX(double xVal);
+//void setY(double yVal);
+
+Point(double xVal, double yVal);
     private:
         double x;
         double y;
